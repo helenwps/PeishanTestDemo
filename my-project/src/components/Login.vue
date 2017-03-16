8e097f34d73364fea7963665d79576bd144e7b75
吴佩珊 <2885542001@qq.com>
2017-03-16 10:18:15 +0800

    文件提交

---
+++ my-project/src/components/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="login">
+    <h1>HELLO MY APP</h1>
+    <span>Message is:{{ msg }}</span>
+    <input type="text" v-model="msg" placeholder="edit me">
+    <router-link to="/register">糟糕！没有账号，赶紧注册一个。</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ my-project/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import Login from '@/components/Login'
+import Register from '@/components/Register'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    }
+  ]
+})
